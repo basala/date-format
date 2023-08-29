7d50ed936f64323f1756fb36c95139b214e41b0c
kira <kira@fanruan.com>
2023-08-29 18:30:55 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,57 @@ export function formate(
   formatter: string | ((dateInfo: DateInfo) => string),
   isPad = false
 ): string {
-  return "";
+  if (isNaN(date.getTime())) {
+    return "Invalid Date";
+  }
+
+  const padStart = (str: number, length: number): string => {
+    const value = str.toString();
+    return isPad ? value.padStart(length, "0") : value;
+  };
+
+  const year = date.getFullYear();
+  const yyyy = padStart(year, 4);
+  const month = date.getMonth() + 1;
+  const MM = padStart(month, 2);
+  const day = date.getDate();
+  const dd = padStart(day, 2);
+  const hour = date.getHours();
+  const hh = padStart(hour, 2);
+  const minute = date.getMinutes();
+  const mm = padStart(minute, 2);
+  const second = date.getSeconds();
+  const ss = padStart(second, 2);
+
+  if (typeof formatter === "string") {
+    switch (formatter) {
+      case "date":
+        return `${yyyy}-${MM}-${dd}`;
+      case "datetime":
+        return `${yyyy}-${MM}-${dd} ${hh}:${mm}:${ss}`;
+      default:
+        return formatter
+          .replaceAll("yyyy", yyyy)
+          .replaceAll("MM", MM)
+          .replaceAll("dd", dd)
+          .replaceAll("hh", hh)
+          .replaceAll("mm", mm)
+          .replaceAll("ss", ss);
+    }
+  }
+
+  return formatter({
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    second,
+    yyyy,
+    MM,
+    dd,
+    hh,
+    mm,
+    ss,
+  });
 }
